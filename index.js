926454d4dc6128e74153b0878b7b4dbf7046ae92
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-04-17 10:11:05 +0600

    add javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+const number = [5,7,  9, 10, 14,27]
+
+const fristNumber =  (val, ind, arr) =>{
+
+    if(val*2===20){
+       return val;
+    }
+
+}
+
+const eventNumber = number.find(fristNumber);
+
+document.write(eventNumber);
